37830ea9166a424a586a58d2d0bd923d1f8bb3de
Joy <a556987@gmail.com>
2019-04-13 21:57:17 +0800

    拆開黑鍵

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@
 $(function(){
 
     //等資源載入完成後才顯示畫面
-    window.onload = function(){
+    /*window.onload = function(){
         $('.loading').hide();
-    };
+    };*/
 
     //變數準備
     $piano = $('.piano');
@@ -24,18 +24,48 @@ $(function(){
         colors = ['#828282', '#AD766A', '#D5404A', '#FFB11B', '#90B44B', '#58B2DC', '#005CAF',  '#A8497A', '#E87A90'];
         
         key_ivory = ['A0', 'B0', 'C1', 'D1', 'E1', 'F1', 'G1', 'A1', 'B1', 'C2', 'D2', 'E2', 'F2', 'G2', 'A2', 'B2', 'C3', 'D3', 'E3', 'F3', 'G3', 'A3', 'B3', 'C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5', 'D5', 'E5', 'F5', 'G5', 'A5', 'B5', 'C6', 'D6', 'E6', 'F6', 'G6', 'A6', 'B6', 'C7', 'D7', 'E7', 'F7', 'G7', 'A7', 'B7', 'C8'];
-        key_ebony = ['Bb0', 'Db1', 'Eb1', 'Gb1', 'Ab1', 'Bb1', 'Db2', 'Eb2', 'Gb2', 'Ab2', 'Bb2', 'Db3', 'Eb3', 'Gb3', 'Ab3', 'Bb3', 'Db4', 'Eb4', 'Gb4', 'Ab4', 'Bb4', 'Db5', 'Eb5', 'Gb5', 'Ab5', 'Bb5', 'Db6', 'Eb6', 'Gb6', 'Ab6', 'Bb6', 'Db7', 'Eb7', 'Gb7', 'Ab7', 'Bb7'];
+        key_ebony_b = ['Bb0', 'Db1', 'Eb1', 'Gb1', 'Ab1', 'Bb1', 'Db2', 'Eb2', 'Gb2', 'Ab2', 'Bb2', 'Db3', 'Eb3', 'Gb3', 'Ab3', 'Bb3', 'Db4', 'Eb4', 'Gb4', 'Ab4', 'Bb4', 'Db5', 'Eb5', 'Gb5', 'Ab5', 'Bb5', 'Db6', 'Eb6', 'Gb6', 'Ab6', 'Bb6', 'Db7', 'Eb7', 'Gb7', 'Ab7', 'Bb7'];
         
         // audio = ['Piano.ff.A0.mp3', 'Piano.ff.Bb0.mp3', 'Piano.ff.B0.mp3', 'Piano.ff.C1.mp3', 'Piano.ff.Db1.mp3', 'Piano.ff.D1.mp3', 'Piano.ff.Eb1.mp3', 'Piano.ff.E1.mp3', 'Piano.ff.F1.mp3', 'Piano.ff.Gb1.mp3', 'Piano.ff.G1.mp3', 'Piano.ff.Ab1.mp3', 'Piano.ff.A1.mp3', 'Piano.ff.Bb1.mp3', 'Piano.ff.B1.mp3', 'Piano.ff.C2.mp3', 'Piano.ff.Db2.mp3', 'Piano.ff.D2.mp3', 'Piano.ff.Eb2.mp3', 'Piano.ff.E2.mp3', 'Piano.ff.F2.mp3', 'Piano.ff.Gb2.mp3', 'Piano.ff.G2.mp3', 'Piano.ff.Ab2.mp3', 'Piano.ff.A2.mp3', 'Piano.ff.Bb2.mp3', 'Piano.ff.B2.mp3', 'Piano.ff.C3.mp3', 'Piano.ff.Db3.mp3', 'Piano.ff.D3.mp3', 'Piano.ff.Eb3.mp3', 'Piano.ff.E3.mp3', 'Piano.ff.F3.mp3', 'Piano.ff.Gb3.mp3', 'Piano.ff.G3.mp3', 'Piano.ff.Ab3.mp3', 'Piano.ff.A3.mp3', 'Piano.ff.Bb3.mp3', 'Piano.ff.B3.mp3', 'Piano.ff.C4.mp3', 'Piano.ff.Db4.mp3', 'Piano.ff.D4.mp3', 'Piano.ff.Eb4.mp3', 'Piano.ff.E4.mp3', 'Piano.ff.F4.mp3', 'Piano.ff.Gb4.mp3', 'Piano.ff.G4.mp3', 'Piano.ff.Ab4.mp3', 'Piano.ff.A4.mp3', 'Piano.ff.Bb4.mp3', 'Piano.ff.B4.mp3', 'Piano.ff.C5.mp3', 'Piano.ff.Db5.mp3', 'Piano.ff.D5.mp3', 'Piano.ff.Eb5.mp3', 'Piano.ff.E5.mp3', 'Piano.ff.F5.mp3', 'Piano.ff.Gb5.mp3', 'Piano.ff.G5.mp3', 'Piano.ff.Ab5.mp3', 'Piano.ff.A5.mp3', 'Piano.ff.Bb5.mp3', 'Piano.ff.B5.mp3', 'Piano.ff.C6.mp3', 'Piano.ff.Db6.mp3', 'Piano.ff.D6.mp3', 'Piano.ff.Eb6.mp3', 'Piano.ff.E6.mp3', 'Piano.ff.F6.mp3', 'Piano.ff.Gb6.mp3', 'Piano.ff.G6.mp3', 'Piano.ff.Ab6.mp3', 'Piano.ff.A6.mp3', 'Piano.ff.Bb6.mp3', 'Piano.ff.B6.mp3', 'Piano.ff.C7.mp3', 'Piano.ff.Db7.mp3', 'Piano.ff.D7.mp3', 'Piano.ff.Eb7.mp3', 'Piano.ff.E7.mp3', 'Piano.ff.F7.mp3', 'Piano.ff.Gb7.mp3', 'Piano.ff.G7.mp3', 'Piano.ff.Ab7.mp3', 'Piano.ff.A7.mp3', 'Piano.ff.Bb7.mp3', 'Piano.ff.B7.mp3', 'Piano.ff.C8.mp3'];
         audio_ivory = ['Piano.ff.A0.mp3', 'Piano.ff.B0.mp3', 'Piano.ff.C1.mp3', 'Piano.ff.D1.mp3', 'Piano.ff.E1.mp3', 'Piano.ff.F1.mp3', 'Piano.ff.G1.mp3', 'Piano.ff.A1.mp3', 'Piano.ff.B1.mp3', 'Piano.ff.C2.mp3', 'Piano.ff.D2.mp3', 'Piano.ff.E2.mp3', 'Piano.ff.F2.mp3', 'Piano.ff.G2.mp3', 'Piano.ff.A2.mp3', 'Piano.ff.B2.mp3', 'Piano.ff.C3.mp3', 'Piano.ff.D3.mp3', 'Piano.ff.E3.mp3', 'Piano.ff.F3.mp3', 'Piano.ff.G3.mp3', 'Piano.ff.A3.mp3', 'Piano.ff.B3.mp3', 'Piano.ff.C4.mp3', 'Piano.ff.D4.mp3', 'Piano.ff.E4.mp3', 'Piano.ff.F4.mp3', 'Piano.ff.G4.mp3', 'Piano.ff.A4.mp3', 'Piano.ff.B4.mp3', 'Piano.ff.C5.mp3', 'Piano.ff.D5.mp3', 'Piano.ff.E5.mp3', 'Piano.ff.F5.mp3', 'Piano.ff.G5.mp3', 'Piano.ff.A5.mp3', 'Piano.ff.B5.mp3', 'Piano.ff.C6.mp3', 'Piano.ff.D6.mp3', 'Piano.ff.E6.mp3', 'Piano.ff.F6.mp3', 'Piano.ff.G6.mp3', 'Piano.ff.A6.mp3', 'Piano.ff.B6.mp3', 'Piano.ff.C7.mp3', 'Piano.ff.D7.mp3', 'Piano.ff.E7.mp3', 'Piano.ff.F7.mp3', 'Piano.ff.G7.mp3', 'Piano.ff.A7.mp3', 'Piano.ff.B7.mp3', 'Piano.ff.C8.mp3'];
         audio_ebony = ['Piano.ff.Bb0.mp3', 'Piano.ff.Db1.mp3', 'Piano.ff.Eb1.mp3', 'Piano.ff.Gb1.mp3', 'Piano.ff.Ab1.mp3', 'Piano.ff.Bb1.mp3', 'Piano.ff.Db2.mp3', 'Piano.ff.Eb2.mp3', 'Piano.ff.Gb2.mp3', 'Piano.ff.Ab2.mp3', 'Piano.ff.Bb2.mp3', 'Piano.ff.Db3.mp3', 'Piano.ff.Eb3.mp3', 'Piano.ff.Gb3.mp3', 'Piano.ff.Ab3.mp3', 'Piano.ff.Bb3.mp3', 'Piano.ff.Db4.mp3', 'Piano.ff.Eb4.mp3', 'Piano.ff.Gb4.mp3', 'Piano.ff.Ab4.mp3', 'Piano.ff.Bb4.mp3', 'Piano.ff.Db5.mp3', 'Piano.ff.Eb5.mp3', 'Piano.ff.Gb5.mp3', 'Piano.ff.Ab5.mp3', 'Piano.ff.Bb5.mp3', 'Piano.ff.Db6.mp3', 'Piano.ff.Eb6.mp3', 'Piano.ff.Gb6.mp3', 'Piano.ff.Ab6.mp3', 'Piano.ff.Bb6.mp3', 'Piano.ff.Db7.mp3', 'Piano.ff.Eb7.mp3', 'Piano.ff.Gb7.mp3', 'Piano.ff.Ab7.mp3', 'Piano.ff.Bb7.mp3'];
 
-        //黑鍵
-        ebony_bre = '<div class="ebony bre"><span>b2</span></div>';
-        ebony_bmi = '<div class="ebony bmi"><span>b3</span></div>';
-        ebony_bsol= '<div class="ebony bsol"><span>b5</span></div>';
-        ebony_bla= '<div class="ebony bla"><span>b6</span></div>';
-        ebony_bsi= '<div class="ebony bsi"><span>b7</span></div>';
+        //黑鍵(容器)
+        ebony = '<div class="ebony"></div>';
+        ebony_1 = '<div class="ebony_1"></div>';
+        ebony_2 = '<div class="ebony_2"></div>';
+        ebony_3 = '<div class="ebony_3"></div>';
+        ebony_4 = '<div class="ebony_4"></div>';
+        ebony_5 = '<div class="ebony_5"></div>';
+
+        //黑鍵(降)
+        ebony_bre = '<div class="ebony_child bre"><span>b2</span></div>';
+        ebony_bmi = '<div class="ebony_child bmi"><span>b3</span></div>';
+        ebony_bsol = '<div class="ebony_child bsol"><span>b5</span></div>';
+        ebony_bla = '<div class="ebony_child bla"><span>b6</span></div>';
+        ebony_bsi = '<div class="ebony_child bsi"><span>b7</span></div>';
+		
+		//黑鍵(升)
+        ebony_udo = '<div class="ebony_child udo"><span>#1</span></div>';
+        ebony_ure = '<div class="ebony_child ure"><span>#2</span></div>';
+        ebony_ufa = '<div class="ebony_child ufa"><span>#4</span></div>';
+        ebony_usol = '<div class="ebony_child usol"><span>#5</span></div>';
+        ebony_ula = '<div class="ebony_child ula"><span>#6</span></div>';
+		
+		//黑鍵(降)不顯示(配合調性)
+        ebony_bre_2 = '<div class="ebony_child bre_2"><span>2</span></div>';
+        ebony_bmi_2 = '<div class="ebony_child bmi_2"><span>3</span></div>';
+        ebony_bsol_2 = '<div class="ebony_child bsol_2"><span>5</span></div>';
+        ebony_bla_2 = '<div class="ebony_child bla_2"><span>6</span></div>';
+        ebony_bsi_2 = '<div class="ebony_child bsi_2"><span>7</span></div>';
+		
+		//黑鍵(升)不顯示(配合調性)
+        ebony_udo_2 = '<div class="ebony_child udo_2"><span>1</span></div>';
+        ebony_ure_2 = '<div class="ebony_child ure_2"><span>2</span></div>';
+        ebony_ufa_2 = '<div class="ebony_child ufa_2"><span>4</span></div>';
+        ebony_usol_2 = '<div class="ebony_child usol_2"><span>5</span></div>';
+        ebony_ula_2 = '<div class="ebony_child ula_2"><span>6</span></div>';
+		
 
         //-------------------
         // 左邊 3 鍵
@@ -55,10 +85,31 @@ $(function(){
             'border-radius': '5px'
         });
 
-        //新增黑鍵
-        $group_left.append(ebony_bsi);
-        $ebony_left_bre = $group_left.find('.bsi');
-        $ebony_left_bre.css({left: '35%'});
+        //新增黑鍵容器
+        $group_left.append(ebony);
+        $ebony = $group_left.find('.ebony');
+        $ebony.css({left: '35%', top: '0'});
+
+        //新增黑鍵(降)
+        $ebony.append(ebony_bsi);
+        
+		//新增黑鍵(升)
+        $ebony.append(ebony_ula);
+        
+		//新增黑鍵(降)不顯示(配合調性)
+        $ebony.append(ebony_bsi_2);
+  
+		//新增黑鍵(升)不顯示(配合調性)
+        $ebony.append(ebony_ula_2);
+        $ebony_left_ula_2 = $ebony.find('.ula_2');
+        $ebony_left_ula_2.css({'border-bottom-left-radius': '5px', 'border-bottom-right-radius': '5px'});
+
+        //上色
+        $group_left.find('.ebony_child').find('span').css({
+            'background': colors[0],
+            'color': '#fff',
+            'display': 'block'
+        });
 
         //新增白鍵內容
         $group_left.find('.ivory').each(function(index){
@@ -70,10 +121,12 @@ $(function(){
 
         //新增黑鍵內容
         $group_left.find('.ebony').each(function(index){
-            //新增 key
-            $(this).append('<div class="key">' + key_ebony[index] + '</div>');
-            //新增音訊
-            $(this).append('<audio id="'+ key_ebony[index] +'"><source src="./audio/'+ audio_ebony[index] +'" type="audio/mpeg"></audio>');
+            $ebony.find('.ebony_child').each(function(index2){
+                //新增 key
+                $(this).append('<div class="key">' + key_ebony_b[index] + '</div>');
+                //新增音訊
+                $(this).append('<audio id="'+ key_ebony_b[index] +'"><source src="./audio/'+ audio_ebony[index] +'" type="audio/mpeg"></audio>');
+            });        
         });
 
         //---------------------
@@ -101,26 +154,76 @@ $(function(){
                 'border-radius': '5px'
             });
 
-            //新增黑鍵
-            $(this).append(ebony_bre);
-            $ebony_bre = $(this).find('.bre');
-            $ebony_bre.css({left: '10%'});
+            //新增黑鍵容器 1
+            $(this).append(ebony_1);
+            $ebony_1 = $(this).find('.ebony_1');
+            $ebony_1.css({left: '10%', top: '0'});
+            //新增黑鍵容器 2
+            $(this).append(ebony_2);
+            $ebony_2 = $(this).find('.ebony_2');
+            $ebony_2.css({left: '25%', top: '0'});
+            //新增黑鍵容器 3
+            $(this).append(ebony_3);
+            $ebony_3 = $(this).find('.ebony_3');
+            $ebony_3.css({left: '53%', top: '0'});
+            //新增黑鍵容器 4
+            $(this).append(ebony_4);
+            $ebony_4 = $(this).find('.ebony_4');
+            $ebony_4.css({left: '67%', top: '0'});
+            //新增黑鍵容器 5
+            $(this).append(ebony_5);
+            $ebony_5 = $(this).find('.ebony_5');
+            $ebony_5.css({left: '82%', top: '0'});
 
-            $(this).append(ebony_bmi);
-            $ebony_bmi = $(this).find('.bmi');
-            $ebony_bmi.css({left: '25%'});
 
-            $(this).append(ebony_bsol);
-            $ebony_bsol = $(this).find('.bsol');
-            $ebony_bsol.css({left: '53%'});
+            //新增黑鍵(降)------------
+            $ebony_1.append(ebony_bre);
+            $ebony_2.append(ebony_bmi);
+            $ebony_3.append(ebony_bsol);
+            $ebony_4.append(ebony_bla);
+            $ebony_5.append(ebony_bsi);
+			
+			//新增黑鍵(升)------------
+            $ebony_1.append(ebony_udo);
+            $ebony_2.append(ebony_ure);
+            $ebony_3.append(ebony_ufa);
+            $ebony_4.append(ebony_usol);
+            $ebony_5.append(ebony_ula);
+			
+			//黑鍵(降)不顯示(配合調性)------------
+            $ebony_1.append(ebony_bre_2);
+            $ebony_2.append(ebony_bmi_2);
+            $ebony_3.append(ebony_bsol_2);
+            $ebony_4.append(ebony_bla_2);
+            $ebony_5.append(ebony_bsi_2);
+			
+			//黑鍵(升)不顯示(配合調性)
+            $ebony_1.append(ebony_udo_2);
+            $ebony_udo_2 = $ebony_1.find('.udo_2');
+            $ebony_udo_2.css({'border-bottom-left-radius': '5px', 'border-bottom-right-radius': '5px'});
 
-            $(this).append(ebony_bla);
-            $ebony_bla = $(this).find('.bla');
-            $ebony_bla.css({left: '67%'});
+            $ebony_2.append(ebony_ure_2);
+            $ebony_ure_2 = $ebony_2.find('.ure_2');
+            $ebony_ure_2.css({'border-bottom-left-radius': '5px', 'border-bottom-right-radius': '5px'});
 
-            $(this).append(ebony_bsi);
-            $ebony_bsi = $(this).find('.bsi');
-            $ebony_bsi.css({left: '82%'});
+            $ebony_3.append(ebony_ufa_2);
+            $ebony_ufa_2 = $ebony_3.find('.ufa_2');
+            $ebony_ufa_2.css({'border-bottom-left-radius': '5px', 'border-bottom-right-radius': '5px'});
+
+            $ebony_4.append(ebony_usol_2);
+            $ebony_usol_2 = $ebony_4.find('.usol_2');
+            $ebony_usol_2.css({'border-bottom-left-radius': '5px', 'border-bottom-right-radius': '5px'});
+
+            $ebony_5.append(ebony_ula_2);
+            $ebony_ula_2 = $ebony_5.find('.ula_2');
+            $ebony_ula_2.css({'border-bottom-left-radius': '5px', 'border-bottom-right-radius': '5px'});
+
+            //上色
+            $(this).find('.ebony_child').find('span').css({
+                'background': colors[index+1],
+                'color': '#fff',
+                'display': 'block'
+            });
 
             //新增白鍵內容
             $(this).find('.ivory').each(function(index2){
@@ -131,11 +234,45 @@ $(function(){
             });
 
             //新增黑鍵內容
-            $(this).find('.ebony').each(function(index2){
-                //新增 key
-                $(this).append('<div class="key">' + key_ebony[1+(5*index)+index2] + '</div>');
-                //新增音訊
-                $(this).append('<audio id="'+ key_ebony[1+(5*index)+index2] +'"><source src="./audio/'+ audio_ebony[1+(5*index)+index2] +'" type="audio/mpeg"></audio>');
+            $(this).find('.ebony_1').each(function(index2){
+                $(this).find('.ebony_child').each(function(index3){
+                    //新增 key
+                    $(this).append('<div class="key">' + key_ebony_b[1+(5*index)+index2] + '</div>'); //第一個黑鍵+(一組5個黑鍵)+現在是第幾個黑鍵
+                    //新增音訊
+                    $(this).append('<audio id="'+ key_ebony_b[1+(5*index)+index2] +'"><source src="./audio/'+ audio_ebony[1+(5*index)+index2] +'" type="audio/mpeg"></audio>');
+                });
+            });
+            $(this).find('.ebony_2').each(function(index2){
+                $(this).find('.ebony_child').each(function(index3){
+                    //新增 key
+                    $(this).append('<div class="key">' + key_ebony_b[1+(5*index)+index2] + '</div>'); //第一個黑鍵+(一組5個黑鍵)+現在是第幾個黑鍵
+                    //新增音訊
+                    $(this).append('<audio id="'+ key_ebony_b[1+(5*index)+index2] +'"><source src="./audio/'+ audio_ebony[1+(5*index)+index2] +'" type="audio/mpeg"></audio>');
+                });
+            });
+            $(this).find('.ebony_3').each(function(index2){
+                $(this).find('.ebony_child').each(function(index3){
+                    //新增 key
+                    $(this).append('<div class="key">' + key_ebony_b[1+(5*index)+index2] + '</div>'); //第一個黑鍵+(一組5個黑鍵)+現在是第幾個黑鍵
+                    //新增音訊
+                    $(this).append('<audio id="'+ key_ebony_b[1+(5*index)+index2] +'"><source src="./audio/'+ audio_ebony[1+(5*index)+index2] +'" type="audio/mpeg"></audio>');
+                });
+            });
+            $(this).find('.ebony_4').each(function(index2){
+                $(this).find('.ebony_child').each(function(index3){
+                    //新增 key
+                    $(this).append('<div class="key">' + key_ebony_b[1+(5*index)+index2] + '</div>'); //第一個黑鍵+(一組5個黑鍵)+現在是第幾個黑鍵
+                    //新增音訊
+                    $(this).append('<audio id="'+ key_ebony_b[1+(5*index)+index2] +'"><source src="./audio/'+ audio_ebony[1+(5*index)+index2] +'" type="audio/mpeg"></audio>');
+                });
+            });
+            $(this).find('.ebony_5').each(function(index2){
+                $(this).find('.ebony_child').each(function(index3){
+                    //新增 key
+                    $(this).append('<div class="key">' + key_ebony_b[1+(5*index)+index2] + '</div>'); //第一個黑鍵+(一組5個黑鍵)+現在是第幾個黑鍵
+                    //新增音訊
+                    $(this).append('<audio id="'+ key_ebony_b[1+(5*index)+index2] +'"><source src="./audio/'+ audio_ebony[1+(5*index)+index2] +'" type="audio/mpeg"></audio>');
+                });
             });
         });
 
@@ -275,15 +412,17 @@ $(function(){
         });
 
         $('.ebony').each(function(index){
-            $(this).on('touchstart', function(event){
-                $(this).css({'background': '#444'});
-                //紀錄簡譜
-                note($(this).find('span').text(), $(this).prevAll('.ivory').find('span').css('background-color'));
-                //播放音訊
-                playaudio($(this).find('.key').text());
-            })
-            .on('touchend', function(event){
-                $(this).css({'background': '#000'});
+            $('.ebony_child').each(function(index){
+                $(this).on('touchstart', function(event){
+                    $(this).css({'background': '#444'});
+                    //紀錄簡譜
+                    note($(this).find('span').text(), $(this).find('span').css('background-color'));
+                    //播放音訊
+                    playaudio($(this).find('.key').text());
+                })
+                .on('touchend', function(event){
+                    $(this).css({'background': '#000'});
+                });
             });
         });
 
@@ -333,15 +472,17 @@ $(function(){
         });
 
         $('.ebony').each(function(index){
-            $(this).on('mousedown', function(event){
-                $(this).css({'background': '#444'});
-                //紀錄簡譜
-                note($(this).find('span').text(), $(this).prevAll('.ivory').find('span').css('background-color'));
-                //播放音訊
-                playaudio($(this).find('.key').text());
-            })
-            .on('mouseup', function(event){
-                $(this).css({'background': '#000'});
+            $('.ebony_child').each(function(index){
+                $(this).on('mousedown', function(event){
+                    $(this).css({'background': '#444'});
+                    //紀錄簡譜
+                    note($(this).find('span').text(), $(this).find('span').css('background-color'));
+                    //播放音訊
+                    playaudio($(this).find('.key').text());
+                })
+                .on('mouseup', function(event){
+                    $(this).css({'background': '#000'});
+                });
             });
         });
 
